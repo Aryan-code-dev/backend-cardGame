ba8b4fa10301d2577c8aa5d226065749a3a48850
Aryan Dande <86364775+Aryan-code-dev@users.noreply.github.com>
2024-03-03 01:54:07 +0530

    atlas con

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const DB_NAME = process.env.DB_NAME;
+const password = process.env.password;
+const uname = process.env.uname
 
 
 
-mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`
-).then(() => console.log("Mongo DB Connected")
-).catch((err) => console.log("Mongo DB Error:", err));
+const con_string = 'mongodb+srv://'+uname+':'+password+'@cluster0.dnwerzh.mongodb.net/card?retryWrites=true&w=majority';
 
+mongoose.connect(con_string);
